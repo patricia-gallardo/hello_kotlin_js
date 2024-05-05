91d7adad32059b904e1e0fab5f641614ef18d945
Patricia Aas <patricia@turtlesec.com>
2024-05-05 14:12:09 +0200

    What I assume is more modern syntax because I borrowed it from the docs
    https://kotlinlang.org/docs/running-kotlin-js.html

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,12 +33,20 @@ kotlin {
         binaries.executable()
     }
 
-    sourceSets["commonMain"].dependencies {
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    }
-
-    sourceSets["jsMain"].dependencies {
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.6.4")
-        implementation("org.jetbrains.kotlin-wrappers:kotlin-node:18.14.0-pre.504")
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+            }
+        }
+        val jsMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.6.4")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-node:18.14.0-pre.504")
+            }
+        }
+        commonTest.dependencies {
+            implementation(kotlin("test")) // Brings all the platform dependencies automatically
+        }
     }
 }
