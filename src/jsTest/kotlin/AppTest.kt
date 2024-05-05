c644875b4dfe28f441d0ff0ad8c98a7dd7e68428
Patricia Aas <patricia@turtlesec.com>
2024-05-05 14:39:00 +0200

    Yay, failing test fail CI
    Remove failing test

--- src/jsTest/kotlin/AppTest.kt
+++ src/jsTest/kotlin/AppTest.kt
@@ -6,9 +6,4 @@ class AppTest {
     fun thingsShouldWork() {
         assertEquals(listOf(1,2,3).reversed(), listOf(3,2,1))
     }
-
-    @Test
-    fun thingsShouldBreak() {
-        assertEquals(listOf(1,2,3).reversed(), listOf(1,2,3))
-    }
 }
